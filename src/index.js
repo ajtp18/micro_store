06d6b3a0d7e9db15022272c67e424d569ab7232e
Armando Tinoco <84752778+ajtp18@users.noreply.github.com>
2023-05-05 23:26:33 -0500

    nuevas caracteristicas
    Se agrego:
    index.js para manejar las caracteristicas del servidor.
    app.js: para controlar la subida del servidor mediante variables de entorno
    
    se agregaron al package.json:
    express para crear la api
    mongoose para la base de datos
    morgan para tener un registro de status.codes desde la consola
    
    dotenv y nodemon en variables de desarrollo
    
    se agrego el npm run dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-src/nodemon
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+app.listen(process.env.PORT);
+console.log(`http://${process.env.HOST}:${process.env.PORT}/`);
